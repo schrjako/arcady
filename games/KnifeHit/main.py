c6b42b37faf69312c8857aff74573d8c089900bb
TroiGDev <zapis.troi@gmail.com>
2025-04-11 16:05:35 +0200

    added in up to date standalone knifehit version

--- games/KnifeHit/main.py
+++ games/KnifeHit/main.py
@@ -1,3 +1,9 @@
+import os
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
 #by default not on gimvic computers
 import pygame
 import numpy as np
@@ -15,8 +21,7 @@ import Sound
 
 #------------------------------------------------------------------------------------------------------
 
-#todo - in game settings for volume
-musicVolume = 0.6
+musicVolume = 0.4
 soundVolume = 0.4
 
 #------------------------------------------------------------------------------------------------------
@@ -234,16 +239,28 @@ while running:
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
+                
+                #get press position and determine if settings pressed or knife thrown
+                mPos = pygame.mouse.get_pos()
 
-                #throw all not yet thrown knives
-                for knife in gs.knives:
-                    knife.throw(gs.knifeThrowForce)
-                    gs.soundMngr.playFromSounds(gs.soundMngr.knifeThrowSfx)
+                if mPos[0] < 30 and mPos[1] < 30:
+                    #toggle music
+                    gs.soundMngr.toggleMusic()
 
-                #spawn new knife 
-                gs.knifeBar.knivesLeft -= 1
-                if gs.knifeBar.knivesLeft > 0:
-                    newKnife = GameObjects.Knife(gs, 200, 600)
+                elif mPos[0] < 75 and mPos[1] < 30:
+                    #toggle sound
+                    gs.soundMngr.toggleSound()
+
+                else:
+                    #throw all not yet thrown knives
+                    for knife in gs.knives:
+                        knife.throw(gs.knifeThrowForce)
+                        gs.soundMngr.playFromSounds(gs.soundMngr.knifeThrowSfx)
+
+                    #spawn new knife 
+                    gs.knifeBar.knivesLeft -= 1
+                    if gs.knifeBar.knivesLeft > 0:
+                        newKnife = GameObjects.Knife(gs, 200, 600)
 
     #update log
     gs.log.updateRotation(dTs)
@@ -282,6 +299,9 @@ while running:
     for particle in gs.particles:
         particle.update(screen)
     gs.particles = [particle for particle in gs.particles if particle.lifeTime > 0]
+
+    #display music/sound ui
+    gs.soundMngr.displayIcons(screen)
 
     #update transition animation
     gs.transition.updateOverlay()
