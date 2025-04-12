74bbcf213673da0c82df2e266284f86d0c27e9b3
Gregor Potočnik <gregor.pot.gm@gmail.com>
2025-04-12 15:07:51 +0200

    Game over screen added

--- games/snake/main.py
+++ games/snake/main.py
@@ -6,14 +6,21 @@ from .spawnables import SpawnableManager, Apple, Bomb
 from .collisionManager import CollisionManager
 
 import random
+from enum import Enum
 
 # The article about hexagonal grids I used: https://www.redblobgames.com/grids/hexagons/
+
+path = "./games/snake"
 
 
 class SnakeGame:
 	"""
 	The main game class handling initialization, game loop, and rendering for the snake game.
 	"""
+
+	class States(Enum):
+		NORMAL = 0
+		GAME_OVER = 1
 
 	def __init__(
 		self,
@@ -46,9 +53,47 @@ class SnakeGame:
 		)
 
 		self.running: bool = True
+		self.state: SnakeGame.States = self.States.NORMAL
+		self.game_over_msg = ""
 
 	def game_over(self, message: str) -> None:
-		self.running = False
+		self.state = self.States.GAME_OVER
+		self.game_over_msg = message
+
+	def draw_game_over(self, message: str) -> None:
+		color = (255, 0, 160)
+
+		# Draw dark overlay
+		overlay = pygame.Surface((self.screen.get_width(), self.screen.get_height()))
+		overlay.set_alpha(150)
+		overlay.fill((0, 0, 0))
+		self.screen.blit(overlay, (0, 0))
+
+		# Create fonts
+		large_font = pygame.font.Font(path + "/fonts/PressStart2P.ttf", 46)
+		small_font = pygame.font.Font(path + "/fonts/PressStart2P.ttf", 20)
+		
+		# Render the main Game Over message.
+		game_over_text = large_font.render("GAME OVER", True, color)
+		game_over_rect = game_over_text.get_rect(
+			center=(self.screen.get_width() // 2, self.screen.get_height() // 2)
+		)
+		self.screen.blit(game_over_text, game_over_rect)
+		
+		# Render the message
+		message_text = small_font.render(message, True, color)
+		extra_rect = message_text.get_rect(
+			center=(self.screen.get_width() // 2, self.screen.get_height() // 2 + 60)
+		)
+		self.screen.blit(message_text, extra_rect)
+
+		# Render the "Press Enter to Continue" text
+		message_text = small_font.render("Press Enter to Continue", True, color)
+		extra_rect = message_text.get_rect(
+			center=(self.screen.get_width() // 2, self.screen.get_height() // 2 - 60)
+		)
+		self.screen.blit(message_text, extra_rect)
+		
 
 	def run(self) -> None:
 		"""
@@ -60,31 +105,38 @@ class SnakeGame:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					self.running = False
-				if event.type == pygame.KEYDOWN:
-					if event.key == pygame.K_LEFT:
-						self.snake.turn_left()
-					if event.key == pygame.K_RIGHT:
-						self.snake.turn_right()
 
-			# Move snake
-			if frame % 10 == 0:
-				self.snake.move()
+				if self.state == self.States.NORMAL:
+					if event.type == pygame.KEYDOWN:
+						if event.key == pygame.K_LEFT:
+							self.snake.turn_left()
+						if event.key == pygame.K_RIGHT:
+							self.snake.turn_right()
 
-			# Spawn apples
-			if frame % 120 == 0:
-				if random.randint(0, len(self.spawnable_manager.get(Apple))) == 0:
-					self.spawnable_manager.spawn_random(Apple)
+				if self.state == self.States.GAME_OVER:
+					if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+						self.running = False
 
-			# Spawn bombs
-			if frame % 120 == 0:
-				if random.randint(0, len(self.spawnable_manager.get(Bomb))) == 0:
-					self.spawnable_manager.spawn_random(Bomb)
+			if self.state == self.States.NORMAL:
+				# Move snake
+				if frame % 10 == 0:
+					self.snake.move()
 
-			# Update spawnables
-			self.spawnable_manager.update()
+				# Spawn apples
+				if frame % 120 == 0:
+					if random.randint(0, len(self.spawnable_manager.get(Apple))) == 0:
+						self.spawnable_manager.spawn_random(Apple)
 
-			# Handle collisions
-			self.collision_manager.handle_collisions()
+				# Spawn bombs
+				if frame % 120 == 0:
+					if random.randint(0, len(self.spawnable_manager.get(Bomb))) == 0:
+						self.spawnable_manager.spawn_random(Bomb)
+
+				# Update spawnables
+				self.spawnable_manager.update()
+
+				# Handle collisions
+				self.collision_manager.handle_collisions()
 
 			# Draw everything
 			self.screen.fill(self.backgound)
@@ -92,6 +144,9 @@ class SnakeGame:
 			self.board.draw(self.screen)
 			self.snake.draw(self.screen)
 			self.spawnable_manager.draw(self.screen)
+			
+			if self.state == self.States.GAME_OVER:
+				self.draw_game_over(self.game_over_msg)
 
 			pygame.display.flip()
 			self.clock.tick(60)
