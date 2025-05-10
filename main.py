fb3b0abd183eaafc3704bb2f9a506e4ee5ef4676
Matic Strgar <matic.strgar09@gmail.com>
2025-05-10 16:20:18 +0200

    2048 game added

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from menu.menu import show_menu
 
 import games.snake.main as snake
 import games.spacepunk.main as spacepunk
-import games.KnifeHit.main as knifehit;
+import games.KnifeHit.main as knifehit
+import games.game_2048.main as game_2048
 
 
 def main():
@@ -13,13 +14,15 @@ def main():
 
 	running = True
 	while running:
-		choice = show_menu(screen, ["snake","spacepunk", "knifehit"])
+		choice = show_menu(screen, ["snake", "spacepunk", "knifehit", "2048 game"])
 		if choice == "snake":
 			snake.run(screen)
-		if choice == "spacepunk":
+		elif choice == "spacepunk":
 			spacepunk.run(screen)
-		if choice == "knifehit":
+		elif choice == "knifehit":
 			knifehit.run(screen)
+		elif choice == "2048 game":
+			game_2048.run(screen)
 		elif choice == "quit":
 			running = False
 
