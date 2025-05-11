2065a96e5446505eddad095e5394cc5accc7316f
Gregor Potočnik <gregor.pot.gm@gmail.com>
2025-05-11 09:59:47 +0200

    reworked best score saving and cosmetic fixes

--- games/game_2048/main.py
+++ games/game_2048/main.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import os
+from pathlib import Path
 
 pygame.init()
 
@@ -34,21 +35,32 @@ COLORS = {
 
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("2048") # title
+pygame.display.set_caption("2048")  # title
+
 
 def load_best_score():
-    if os.path.exists("best_score.txt"):
-        with open("best_score.txt", "r") as file:
+    path = str(Path(__file__).parent / ".SCORES_2048.txt")
+    if os.path.exists(path):
+        with open(path, "r") as file:
             try:
                 return int(file.read())
             except ValueError:
                 return 0
     return 0
 
+
 def save_best_score(score):
-    with open("best_score.txt", "w") as file:
+    path = str(Path(__file__).parent / ".SCORES_2048.txt")
+    with open(path, "w") as file:
         file.write(str(score))
-        
+
+
+def draw_scores(score):
+    score_text = FONT.render(f"Score: {score}", True, (255, 255, 255))
+    best_text = FONT.render(f"Best: {load_best_score()}", True, (255, 255, 255))
+    screen.blit(score_text, (10, HEIGHT - 40))
+    screen.blit(best_text, (WIDTH - best_text.get_width() - 10, HEIGHT - 40))
+
 
 # grid dispay
 def draw_grid(grid, score):
@@ -57,50 +69,50 @@ def draw_grid(grid, score):
         for col in range(GRID_SIZE):
             value = grid[row][col]
             color = COLORS.get(value, (60, 58, 50))
-            pygame.draw.rect(screen, color, (col*TILE_SIZE, row*TILE_SIZE, TILE_SIZE, TILE_SIZE), border_radius = 10)
-            pygame.draw.rect(screen, (150, 140, 130), (col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE), width = 2, border_radius = 10)
+            pygame.draw.rect(screen, color, (col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE), border_radius=10)
+            pygame.draw.rect(
+                screen,
+                (150, 140, 130),
+                (col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE),
+                width=2,
+                border_radius=10,
+            )
             if value != 0:
                 text = FONT.render(str(value), True, (0, 0, 0))
-                text_rect = text.get_rect(center=(col*TILE_SIZE + TILE_SIZE//2, row*TILE_SIZE + TILE_SIZE//2))
+                text_rect = text.get_rect(center=(col * TILE_SIZE + TILE_SIZE // 2, row * TILE_SIZE + TILE_SIZE // 2))
                 screen.blit(text, text_rect)
-                
-                
-    # scores
-    score_text = FONT.render(f"Score: {score}", True, (255, 255, 255))
-    best_text = FONT.render(f"Best: {load_best_score()}", True, (255, 255, 255))
-    screen.blit(score_text, (10, HEIGHT - 40))
-    screen.blit(best_text, (WIDTH-130, HEIGHT -40))
-    
+
+    draw_scores(score)
+
     pygame.display.update()
-                
- 
+
+
 def add_new(grid, score):
     while True:
         row = random.randint(0, 3)
         col = random.randint(0, 3)
         if grid[row][col] == 0:
             break
-        
+
     value = 2
     grid[row][col] = value
     draw_grid(grid, score)
- 
-                
-def  if_win(grid):
+
+
+def if_win(grid):
     # check if win
     c = 0
     for row in range(GRID_SIZE):
         for col in range(GRID_SIZE):
             value = grid[row][col]
             if value != 0:
-                c+=1
-                
+                c += 1
+
             if value == 2048:
                 return "end_win"
-            
-    if c == GRID_SIZE*GRID_SIZE:
-        return "game_over"
 
+    if c == GRID_SIZE * GRID_SIZE:
+        return "game_over"
 
 
 def move_left(grid):
@@ -110,28 +122,28 @@ def move_left(grid):
         for col in range(GRID_SIZE):
             if grid[row][col] != 0:
                 current_col = col
-                while current_col > 0 and grid[row][current_col-1] == 0:
-                    grid[row][current_col-1] = grid[row][current_col]
+                while current_col > 0 and grid[row][current_col - 1] == 0:
+                    grid[row][current_col - 1] = grid[row][current_col]
                     grid[row][current_col] = 0
-                    current_col -=1
+                    current_col -= 1
                     moved = True
 
-    # merge if nessesery
+        # merge if nessesery
         for col in range(1, GRID_SIZE):
-            if grid[row][col-1] == grid[row][col] and grid[row][col-1] != 0:
-                grid[row][col-1] *= 2
-                score_gain += grid[row][col-1]
+            if grid[row][col - 1] == grid[row][col] and grid[row][col - 1] != 0:
+                grid[row][col - 1] *= 2
+                score_gain += grid[row][col - 1]
                 grid[row][col] = 0
                 moved = True
-    
+
         for col in range(GRID_SIZE):
             if grid[row][col] != 0:
                 current_col = col
-                while current_col > 0 and grid[row][current_col-1] == 0:
-                    grid[row][current_col-1] = grid[row][current_col]
+                while current_col > 0 and grid[row][current_col - 1] == 0:
+                    grid[row][current_col - 1] = grid[row][current_col]
                     grid[row][current_col] = 0
-                    current_col -=1
-    
+                    current_col -= 1
+
     return moved, score_gain
 
 
@@ -139,33 +151,31 @@ def move_right(grid):
     score_gain = 0
     moved = False
     for row in range(GRID_SIZE):
-        for col in range(GRID_SIZE-2, -1, -1):
+        for col in range(GRID_SIZE - 2, -1, -1):
             if grid[row][col] != 0:
                 current_col = col
-                while current_col < GRID_SIZE -1 and grid[row][current_col+1] == 0:
-                    grid[row][current_col+1] = grid[row][current_col]
+                while current_col < GRID_SIZE - 1 and grid[row][current_col + 1] == 0:
+                    grid[row][current_col + 1] = grid[row][current_col]
                     grid[row][current_col] = 0
-                    current_col +=1
+                    current_col += 1
                     moved = True
 
-    # merge if nessesery
+        # merge if nessesery
         for col in range(1, GRID_SIZE):
-            if grid[row][col-1] == grid[row][col] and grid[row][col-1] != 0:
-                grid[row][col-1] *= 2
-                score_gain += grid[row][col-1]
+            if grid[row][col - 1] == grid[row][col] and grid[row][col - 1] != 0:
+                grid[row][col - 1] *= 2
+                score_gain += grid[row][col - 1]
                 grid[row][col] = 0
                 moved = True
-    
-        
-        for col in range(GRID_SIZE-2, -1, -1):
+
+        for col in range(GRID_SIZE - 2, -1, -1):
             if grid[row][col] != 0:
                 current_col = col
-                while current_col < GRID_SIZE -1 and grid[row][current_col+1] == 0:
-                    grid[row][current_col+1] = grid[row][current_col]
+                while current_col < GRID_SIZE - 1 and grid[row][current_col + 1] == 0:
+                    grid[row][current_col + 1] = grid[row][current_col]
                     grid[row][current_col] = 0
-                    current_col +=1
-                    
-    
+                    current_col += 1
+
     return moved, score_gain
 
 
@@ -176,27 +186,27 @@ def move_up(grid):
         for row in range(1, GRID_SIZE):
             if grid[row][col] != 0:
                 current_row = row
-                while current_row > 0 and grid[current_row-1][col] == 0:
-                    grid[current_row-1][col] = grid[current_row][col]
+                while current_row > 0 and grid[current_row - 1][col] == 0:
+                    grid[current_row - 1][col] = grid[current_row][col]
                     grid[current_row][col] = 0
-                    current_row -=1
+                    current_row -= 1
                     moved = True
 
-    # merge if nessesery
+        # merge if nessesery
         for row in range(1, GRID_SIZE):
-            if grid[row][col] == grid[row-1][col] and grid[row][col] != 0:
-                grid[row-1][col] *= 2
+            if grid[row][col] == grid[row - 1][col] and grid[row][col] != 0:
+                grid[row - 1][col] *= 2
                 score_gain += grid[row][col]
                 grid[row][col] = 0
                 moved = True
-    
+
         for row in range(1, GRID_SIZE):
             if grid[row][col] != 0:
                 current_row = row
-                while current_row > 0 and grid[current_row-1][col] == 0:
-                    grid[current_row-1][col] = grid[current_row][col]
+                while current_row > 0 and grid[current_row - 1][col] == 0:
+                    grid[current_row - 1][col] = grid[current_row][col]
                     grid[current_row][col] = 0
-                    current_row -=1
+                    current_row -= 1
 
     return moved, score_gain
 
@@ -205,52 +215,47 @@ def move_down(grid):
     score_gain = 0
     moved = False
     for col in range(GRID_SIZE):
-        for row in range(GRID_SIZE-2, -1, -1):
+        for row in range(GRID_SIZE - 2, -1, -1):
             if grid[row][col] != 0:
                 current_row = row
-                while current_row < GRID_SIZE-1 and grid[current_row+1][col] == 0:
-                    grid[current_row+1][col] = grid[current_row][col]
+                while current_row < GRID_SIZE - 1 and grid[current_row + 1][col] == 0:
+                    grid[current_row + 1][col] = grid[current_row][col]
                     grid[current_row][col] = 0
-                    current_row +=1
+                    current_row += 1
                     moved = True
 
-    # merge if nessesery
-        for row in range(GRID_SIZE-2, -1, -1):
-            if grid[row][col] == grid[row+1][col] and grid[row][col] != 0:
-                grid[row+1][col] *= 2
+        # merge if nessesery
+        for row in range(GRID_SIZE - 2, -1, -1):
+            if grid[row][col] == grid[row + 1][col] and grid[row][col] != 0:
+                grid[row + 1][col] *= 2
                 score_gain += grid[row][col]
                 grid[row][col] = 0
                 moved = True
-    
-        for row in range(GRID_SIZE-2, -1, -1):
+
+        for row in range(GRID_SIZE - 2, -1, -1):
             if grid[row][col] != 0:
                 current_row = row
-                while current_row < GRID_SIZE-1 and grid[current_row+1][col] == 0:
-                    grid[current_row+1][col] = grid[current_row][col]
+                while current_row < GRID_SIZE - 1 and grid[current_row + 1][col] == 0:
+                    grid[current_row + 1][col] = grid[current_row][col]
                     grid[current_row][col] = 0
-                    current_row +=1
-                    
+                    current_row += 1
+
     return moved, score_gain
 
 
 def show_center_message(message, color=(255, 255, 255)):
     text = FONT.render(message, True, color)
-    text_r = text.get_rect(center=(WIDTH//2, HEIGHT//2))
+    text_r = text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
     screen.blit(text, text_r)
-    
+
     pygame.display.update()
     pygame.time.wait(3000)
-    
 
 
 def main():
     score = 0
     best_score = load_best_score()
-    grid = [
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]]
+    grid = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
 
     # setup
     positions = random.sample([(r, c) for r in range(4) for c in range(4)], 2)
@@ -259,10 +264,9 @@ def main():
         row, col = pos
         value = random.choice(CHOICES)
         grid[row][col] = value
-        
-    draw_grid(grid, score)    
-        
-        
+
+    draw_grid(grid, score)
+
     running = True
     while running:
         for event in pygame.event.get():
@@ -278,62 +282,55 @@ def main():
                     moved, score_gain = move_up(grid)
                 if event.key == pygame.K_DOWN:
                     moved, score_gain = move_down(grid)
-                    
-                
+
                 if moved:
-                    score += score_gain
+                    score += score_gain  # type: ignore
                     draw_grid(grid, score)
                     add_new(grid, score)
-                    
+
         if if_win(grid) == "end_win":
             screen.fill(BACKGROUND_COLOR)
-            score_text = FONT.render(f"Score: {score}", True, (255, 255, 255))
-            screen.blit(score_text, (10, HEIGHT - 40))
-            
+            draw_scores(score)
             show_center_message("You Win!", (255, 215, 0))
             running = False
-            
+
         if if_win(grid) == "game_over":
             screen.fill(BACKGROUND_COLOR)
-            score_text = FONT.render(f"Score: {score}", True, (255, 255, 255))
-            screen.blit(score_text, (10, HEIGHT - 40))
-            
-            show_center_message("Game Over!", (255, 0, 0)) # red
+            draw_scores(score)
+            show_center_message("Game Over!", (255, 0, 0))  # red
             running = False
 
-        if running == False:
+        if not running:
             if score > best_score:
                 save_best_score(score)
-                
-            
+
         pygame.time.delay(100)
-        
-        
+
+
 def run(screen_in: pygame.Surface):
-	global screen
+    global screen
 
-	# Save old data
-	old_dimensions = screen_in.get_size()
-	old_title = pygame.display.get_caption()
+    # Save old data
+    old_dimensions = screen_in.get_size()
+    old_title = pygame.display.get_caption()
 
-	# Resize screen and set new caption
-	screen = pygame.display.set_mode((WIDTH, HEIGHT))
-	pygame.display.set_caption("2048")
+    # Resize screen and set new caption
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("2048")
 
-	# Run the game
-	main()
+    # Run the game
+    main()
 
-	# Set the screen back to the starting stage
-	screen_in = pygame.display.set_mode(old_dimensions)
-	pygame.display.set_caption(*old_title)
+    # Set the screen back to the starting stage
+    screen_in = pygame.display.set_mode(old_dimensions)
+    pygame.display.set_caption(*old_title)
 
 
 # If ran directly (not through menu), this will trigger
 if __name__ == "__main__":
-	screen = pygame.display.set_mode((WIDTH, HEIGHT))
-	pygame.display.set_caption("2048")  # title
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("2048")  # title
 
-	main()  # main function
+    main()  # main function
 
-	pygame.quit()
-    
+    pygame.quit()
